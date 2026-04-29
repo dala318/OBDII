122bd38122e6cdaf30b29e52e0770f0bbbc21582
Daniel Vikström <daniel@vikstrom.name>
2026-04-29 23:00:43 +0200

    Add TransportBLE

--- obdii/transports/__init__.py
+++ obdii/transports/__init__.py
@@ -1,7 +1,9 @@
 from .transport_serial import TransportSerial
 from .transport_socket import TransportSocket
+from .transport_ble import TransportBLE
 
 __all__ = [
     "TransportSerial",
     "TransportSocket",
+    "TransportBLE",
 ]
